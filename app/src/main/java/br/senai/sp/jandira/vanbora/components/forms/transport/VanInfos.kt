ea27582cf115a6269cf95db2a18bb16dc2298f6c
Arthur Augusto <98403987+arthuraugusn@users.noreply.github.com>
2023-03-07 10:54:41 -0300

    commt

--- app/src/main/java/br/senai/sp/jandira/vanbora/components/forms/transport/VanInfos.kt
+++ app/src/main/java/br/senai/sp/jandira/vanbora/components/forms/transport/VanInfos.kt
@@ -1,41 +1,37 @@
-package br.senai.sp.jandira.vanbora.components.forms.transport
-
-import androidx.compose.foundation.Image
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.DirectionsBus
 import androidx.compose.material.icons.filled.PhotoCamera
 import androidx.compose.material.icons.filled.Warning
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.LastBaseline
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.LineHeightStyle
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import br.senai.sp.jandira.vanbora.R
 
-
 @Composable
-fun VanInfos(){
+fun VanInfos(context: Context) {
+
     var placaVan by rememberSaveable {
         mutableStateOf("")
     }
     var modeloVan by rememberSaveable {
         mutableStateOf("")
     }
-
-    //Lembrar de mudar para int quando fizer função
     var vagasVan by rememberSaveable {
         mutableStateOf("")
     }
@@ -50,114 +46,116 @@ fun VanInfos(){
         mutableStateOf(false)
     }
 
+    val scrollState = rememberScrollState()
 
-
-    //Main
+    //IMAGE
     Column(
         modifier = Modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.SpaceAround,
+            .fillMaxWidth()
+            .fillMaxHeight(0.15f)
+            .padding(start = 80.dp, end = 80.dp)
+            .background(Color(156, 156, 156, 255))
+            .clickable {
+                Toast
+                    .makeText(context, "Quem ler é gay kkkk", Toast.LENGTH_SHORT)
+                    .show()
+            },
+        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        IconButton(modifier = Modifier.fillMaxWidth(0.9f).fillMaxHeight(0.25f).background(Color.Blue),onClick = { /*TODO*/ }) {
-            Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
-                Row(horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.CenterVertically, modifier = Modifier.fillMaxWidth().padding(start = 80.dp)) {
-                    Icon(imageVector = Icons.Filled.PhotoCamera, contentDescription = "icone camera", modifier = Modifier.fillMaxSize(0.2f))
-                }
-                Image(imageVector = Icons.Filled.DirectionsBus, contentDescription = "icone pararepresentar a van", modifier = Modifier.fillMaxSize())
-
-            }
-
-        }
-        Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
-            OutlinedTextField(value = placaVan, onValueChange = {placaVan = it}, modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 52.dp, end = 52.dp),
-                label = {
-                    Text(
-                        text = stringResource(id = R.string.placa_van),
-                        textAlign = TextAlign.Center,
-                        style = TextStyle(
-                            color = Color.Black,
-                        )
-                    )
-                },
-                trailingIcon = {
-                    if (isPlacaVanError) Icon(
-                        imageVector = Icons.Default.Warning,
-                        contentDescription = ""
-                    )
-                },
-                isError = isPlacaVanError,
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
-                colors = TextFieldDefaults.outlinedTextFieldColors(
-                    focusedBorderColor = Color(0, 0, 0, 255),
-                    unfocusedBorderColor = Color(0, 0, 0, 255)
-                ))
-            OutlinedTextField(value = modeloVan, onValueChange = {modeloVan = it}, modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 60.dp, start = 52.dp, end = 52.dp),
-                label = {
-                    Text(
-                    text = stringResource(id = R.string.modelo_van),
-                    textAlign = TextAlign.Center,
-                    style = TextStyle(
-                        color = Color.Black,
-                    )
-                    )
-                },
-                trailingIcon = {
-                    if (isModeloVanError) Icon(
-                        imageVector = Icons.Default.Warning,
-                        contentDescription = ""
-                    )
-                },
-                isError = isModeloVanError,
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
-                colors = TextFieldDefaults.outlinedTextFieldColors(
-                    focusedBorderColor = Color(0, 0, 0, 255),
-                    unfocusedBorderColor = Color(0, 0, 0, 255)
-                ))
-            OutlinedTextField(value = vagasVan, onValueChange = {vagasVan = it}, modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 60.dp, start = 52.dp, end = 52.dp),
-                label = {
-                    Text(
-                        text = stringResource(id = R.string.numero_vagas),
-                        textAlign = TextAlign.Center,
-                        style = TextStyle(
-                            color = Color.Black,
-                        )
-                    )
-                },
-                trailingIcon = {
-                    if (isVagasVanError) Icon(
-                        imageVector = Icons.Default.Warning,
-                        contentDescription = ""
-                    )
-                },
-                isError = isVagasVanError,
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
-                colors = TextFieldDefaults.outlinedTextFieldColors(
-                    focusedBorderColor = Color(0, 0, 0, 255),
-                    unfocusedBorderColor = Color(0, 0, 0, 255)
-                ))
-        }
+        Icon(
+            imageVector = Icons.Filled.PhotoCamera,
+            contentDescription = "",
+            modifier = Modifier.fillMaxSize()
+        )
     }
 
-    //Footer
-
-    Button(
-        onClick = {
-            isPlacaVanError = placaVan.isEmpty()
-            isModeloVanError = modeloVan.isEmpty()
-            isVagasVanError = vagasVan.isEmpty()
-        },
-        colors = ButtonDefaults.buttonColors(Color(250, 210, 69, 255))
-
-    ) {
-        Text(
-            text = stringResource(id = R.string.save)
+    //OUTLINED
+    Column(modifier = Modifier.verticalScroll(scrollState)) {
+        OutlinedTextField(
+            value = placaVan,
+            onValueChange = {
+                placaVan = it
+                if (it == "" || it == null) {
+                    isPlacaVanError
+                }
+            },
+            modifier = Modifier.padding(bottom = 20.dp),
+            label = {
+                Text(
+                    text = stringResource(id = R.string.placa_van)
+                )
+            },
+            trailingIcon = {
+                if (isPlacaVanError) Icon(
+                    imageVector = Icons.Default.Warning,
+                    contentDescription = ""
+                )
+            },
+            isError = isPlacaVanError,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
         )
+        OutlinedTextField(
+            value = modeloVan,
+            onValueChange = {
+                modeloVan = it
+                if (it == "" || it == null) {
+                    isModeloVanError
+                }
+            },
+            modifier = Modifier.padding(bottom = 20.dp),
+            label = {
+                Text(
+                    text = stringResource(id = R.string.modelo_van)
+                )
+            },
+            trailingIcon = {
+                if (isModeloVanError) Icon(
+                    imageVector = Icons.Default.Warning,
+                    contentDescription = ""
+                )
+            },
+            isError = isModeloVanError,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
+        )
+        OutlinedTextField(
+            value = vagasVan,
+            onValueChange = {
+                vagasVan = it
+                if (it == "" || it == null) {
+                    isVagasVanError
+                }
+            },
+            label = {
+                Text(
+                    text = stringResource(id = R.string.numero_vagas)
+                )
+            },
+            trailingIcon = {
+                if (isVagasVanError) Icon(
+                    imageVector = Icons.Default.Warning,
+                    contentDescription = ""
+                )
+            },
+            isError = isVagasVanError,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+    }
+
+    //BUTTON
+    Column() {
+        Button(
+            onClick = {
+                isPlacaVanError = placaVan.length == 0
+                isModeloVanError = modeloVan.length == 0
+                isVagasVanError = vagasVan.length == 0
+                      },
+            colors = ButtonDefaults.buttonColors(Color(250, 210, 69, 255))
+
+        ) {
+            Text(
+                text = stringResource(id = R.string.save)
+            )
+        }
     }
 }
--- app/src/main/java/br/senai/sp/jandira/vanbora/ui/activities/VanComplements.kt
+++ app/src/main/java/br/senai/sp/jandira/vanbora/ui/activities/VanComplements.kt
@@ -1,23 +1,30 @@
 package br.senai.sp.jandira.vanbora.ui.activities
 
+import VanInfos
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PhotoCamera
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.senai.sp.jandira.vanbora.R
 import br.senai.sp.jandira.vanbora.components.HeaderSelectDriverComplement
-import br.senai.sp.jandira.vanbora.components.forms.transport.VanInfos
 import br.senai.sp.jandira.vanbora.ui.theme.VanboraTheme
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
@@ -35,39 +42,52 @@ class VanComplements : ComponentActivity() {
                     SideEffect {
                         systemUi.setStatusBarColor(color = Color(255, 255, 255, 0), darkIcons = true)
                     }
-                    Greeting()
+                    VanInfosActivity()
                 }
             }
         }
     }
 }
 
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun Greeting() {
+fun VanInfosActivity(){
 
-    val scrollState = rememberScrollState()
-    Column(modifier = Modifier.fillMaxSize().verticalScroll(scrollState)) {
+    val driverActivityComplements by remember {
+        mutableStateOf(DriverActivityComplements::class.java)
+    }
 
-        val context = LocalContext.current
 
-        val selectActivy by remember {
-            mutableStateOf(SelectActivity::class.java)
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .paint(
+                painter = painterResource(id = R.drawable.background),
+                contentScale = ContentScale.Crop),
+
+    ) {
+
+        //Header
+        Column() {
+            HeaderSelectDriverComplement(
+                context = context,
+                componentActivity = driverActivityComplements.newInstance()
+            )
         }
+        //DIV PAI
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 30.dp),
+            verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
 
+            VanInfos(context)
 
-
-        HeaderSelectDriverComplement(
-            context = context,
-            componentActivity = selectActivy.newInstance()
-        )
-        VanInfos()
+        }
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    VanboraTheme {
-        Greeting()
-    }
-}
