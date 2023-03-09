f958070b0e2864748b8450fda0dc83715d32f57b
leo <leoextreme12345678910@gmail.com>
2023-03-09 07:50:09 -0300

    commit

--- app/src/main/java/br/senai/sp/jandira/vanbora/call_functions/GetFunctionsCall.kt
+++ app/src/main/java/br/senai/sp/jandira/vanbora/call_functions/GetFunctionsCall.kt
@@ -2,7 +2,6 @@ package br.senai.sp.jandira.vanbora.call_functions
 
 import br.senai.sp.jandira.vanbora.api.calls.user.UserCall
 import br.senai.sp.jandira.vanbora.api.retrofit.RetrofitApi
-import retrofit2.Call
 
 class GetFunctionsCall {
 
