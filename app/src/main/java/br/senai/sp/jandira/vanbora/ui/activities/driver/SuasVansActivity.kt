0c3d1ee586d2b97f316e8902ec5edbe04b881ca6
Nicolas Dobbeck Mendes <98404627+NicolasDobbeck@users.noreply.github.com>
2023-04-27 11:24:56 -0300

    Criacao Suas Vans

--- app/src/main/java/br/senai/sp/jandira/vanbora/ui/activities/driver/SuasVansActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/vanbora/ui/activities/driver/SuasVansActivity.kt
@@ -1,16 +1,28 @@
 package br.senai.sp.jandira.vanbora.ui.activities.driver
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import br.senai.sp.jandira.vanbora.call_functions.GetFunctionsCall
+import br.senai.sp.jandira.vanbora.model.driver.DriverList
 import br.senai.sp.jandira.vanbora.ui.activities.driver.ui.theme.VanboraTheme
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SuasVansActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +43,32 @@ class SuasVansActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    var context = LocalContext.current
+
+    var drivers by remember {
+        mutableStateOf(DriverList(listOf()))
+    }
+
+    val driversCall = GetFunctionsCall.getDriverCall().getAllDrivers()
+
+
+    driversCall.enqueue(object : Callback<DriverList> {
+        override fun onResponse(call: Call<DriverList>, response: Response<DriverList>) {
+            drivers = response.body()!!
+        }
+
+        override fun onFailure(call: Call<DriverList>, t: Throwable) {
+            Log.i("ds3m", "onFailure: $t")
+        }
+    })
+
+    Column (
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween
+            ) {
+    }
+
 }
 
 @Preview(showBackground = true)
