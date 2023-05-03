5951f5c7690af9d1cca491a180922c0472f4edd8
Nicolas Dobbeck Mendes <98404627+NicolasDobbeck@users.noreply.github.com>
2023-05-03 10:53:29 -0300

    Create Files

---
+++ app/src/main/java/br/senai/sp/jandira/vanbora/components/driver/footer/FooterDriver.kt
@@ -0,0 +1,115 @@
+package br.senai.sp.jandira.vanbora.components.driver.footer
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import br.senai.sp.jandira.vanbora.components.headers.Destinos
+import br.senai.sp.jandira.vanbora.components.headers.Rotas.*
+
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Preview
+@Composable
+fun FooterDriver() {
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = { BottomBar(navController = navController) }
+    ) {
+        Navigation(navController = navController)
+    }
+}
+
+@Composable
+fun BottomBar(navController: NavHostController) {
+    val screens = listOf(
+        Destinos.RotaMotoristas,
+        Destinos.RotaMeusContratos,
+        Destinos.RotaNotificacoes,
+        Destinos.RotaContate,
+    )
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+
+    BottomNavigation {
+        screens.forEach { screen ->
+            AddItem(
+                screen = screen,
+                currentDestination = currentDestination,
+                navController = navController
+            )
+        }
+    }
+}
+
+@Composable
+fun RowScope.AddItem(
+    screen: Destinos,
+    currentDestination: NavDestination?,
+    navController: NavHostController
+) {
+    BottomNavigationItem(
+        label = {
+            Text(text = screen.title, color = Color.Black)
+        },
+        icon = {
+            Icon(
+                imageVector = screen.ico,
+                contentDescription = "Navigation Icon",
+                tint = Color.Black
+            )
+        },
+        selected = currentDestination?.hierarchy?.any {
+            it.route == screen.route
+        } == true,
+        unselectedContentColor = LocalContentColor.current.copy(alpha = ContentAlpha.disabled),
+        onClick = {
+            navController.navigate(screen.route) {
+                popUpTo(navController.graph.findStartDestination().id)
+                launchSingleTop = true
+            }
+        },
+        modifier = Modifier.background(Color(224, 180, 65, 255))
+    )
+}
+
+@Composable
+fun Navigation(navController: NavHostController) {
+
+    NavHost(navController, startDestination = Destinos.RotaMotoristas.route) {
+
+        composable(Destinos.RotaPerfil.route) {
+            VerPerfil()
+        }
+
+        composable(Destinos.RotaNotificacoes.route) {
+            LocalizeSe()
+        }
+
+        composable(Destinos.RotaMeusContratos.route) {
+            MeusContratos()
+        }
+
+        composable(Destinos.RotaContate.route) {
+            ContateNos()
+        }
+
+        composable(Destinos.RotaMotoristas.route) {
+            Motoristas()
+        }
+
+    }
+}
--- app/src/main/java/br/senai/sp/jandira/vanbora/ui/activities/driver/NotificationPageActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/vanbora/ui/activities/driver/NotificationPageActivity.kt
@@ -4,16 +4,22 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FilterList
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.senai.sp.jandira.vanbora.R
 import br.senai.sp.jandira.vanbora.call_functions.GetFunctionsCall
 import br.senai.sp.jandira.vanbora.model.driver.Driver
@@ -22,7 +28,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class NotificationPageActivity: ComponentActivity() {
+class NotificationPageActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,10 +44,14 @@ class NotificationPageActivity: ComponentActivity() {
             }
         }
     }
-    
+
     @Preview
     @Composable
-    fun NotificationPage (){
+    fun NotificationPage() {
+
+        var filterState by remember {
+            mutableStateOf("")
+        }
 
         val context = LocalContext.current
 
@@ -66,15 +76,69 @@ class NotificationPageActivity: ComponentActivity() {
 
         })
 
-        Column (
+        Column(
             modifier = Modifier
                 .fillMaxSize()
                 .paint(
                     painter = painterResource(id = R.drawable.background2)
                 )
+        ) {
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
+                    OutlinedTextField(
+                        value = filterState,
+                        onValueChange = {
+                            filterState = it
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth(0.8f),
+                        leadingIcon = {
+                            Icon(
+                                imageVector = Icons.Filled.Search,
+                                contentDescription = "",
+                                tint = Color(238, 179, 31, 255)
+                            )
+                        },
+                        shape = RoundedCornerShape(40.dp),
+                        colors = TextFieldDefaults.outlinedTextFieldColors(
+                            focusedBorderColor = Color(208, 173, 82, 255),
+                            unfocusedBorderColor = Color(208, 173, 82, 255),
+                            backgroundColor = Color(255, 248, 228, 255)
+                        )
+                    )
+                }
+                Spacer(modifier = Modifier.padding(6.dp))
 
+                Icon(
+                    imageVector = Icons.Filled.FilterList,
+                    contentDescription = "",
+                    modifier = Modifier
+                        .height(46.dp)
+                        .width(46.dp),
+                    tint = Color.Black
+                )
 
+            }
+
+            Spacer(modifier = Modifier.padding(10.dp))
+
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+
+            }
         }
     }
 }
