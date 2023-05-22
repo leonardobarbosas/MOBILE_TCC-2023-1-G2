f016f0f4ccb7c6c7b22eb9d1e27c2bf4c120147c
Nicolas Dobbeck Mendes <98404627+NicolasDobbeck@users.noreply.github.com>
2023-05-22 11:11:09 -0300

    Update

--- app/src/main/java/br/senai/sp/jandira/vanbora/roomData/dao/LoginDataDao.kt
+++ app/src/main/java/br/senai/sp/jandira/vanbora/roomData/dao/LoginDataDao.kt
@@ -1,7 +1,12 @@
 package br.senai.sp.jandira.vanbora.roomData.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import br.senai.sp.jandira.vanbora.roomData.model.LoginDataEntity
 
 @Dao
 interface LoginDataDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(loginData: LoginDataEntity)
 }
